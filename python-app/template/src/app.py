57a8c26a3a6d37f1a395cbc644d57534fe18528a
clecio.antao <clecio.antao@gmail.com>
2025-06-06 07:32:01 -0300

    feat: primeiro envio

---
+++ python-app/template/src/app.py
@@ -0,0 +1,26 @@
+from flask import Flask, jsonify
+from prometheus_flask_exporter import PrometheusMetrics
+import datetime
+import socket
+
+app = Flask(__name__)
+metrics = PrometheusMetrics(app)
+
+@app.route('/api/v1/info')
+def info():
+    return jsonify({
+        'time': datetime.datetime.now().strftime("%I:%M:%S %p on %B %d, %Y"),
+        'hostname': socket.gethostname(),
+        'message': 'You are doing great, little human! Clecio',
+        'deployed_on': 'kubernetes',
+        'env': '${{values.app_env}}',
+        'app_name': '${{values.app_name}}'
+    })
+
+@app.route('/api/v1/healthz')
+def health():
+    return jsonify({'status': 'up'}), 200
+
+# Prometheus metrics estarão em /metrics
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
